ace967aa6cf4be18e952f2ba66bce10585355043
Igor Leal <igor.leal@izettle.com>
2020-12-07 00:01:53 +0100

    First widget tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,23 +8,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:randomPicker/main.dart';
+import 'package:RandomPicker/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets('Starts with message and hide it once one item is added', (WidgetTester tester) async {
+    await tester.pumpWidget(RandomPickerApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text("Use the button to add items to the list"), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add_circle));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text("Use the button to add items to the list"), findsNothing);
+  });
+
+  testWidgets('Add adds new row', (WidgetTester tester) async {
+    await tester.pumpWidget(RandomPickerApp());
+
+    expect(find.byIcon(Icons.remove_circle), findsNothing);
+
+    await tester.tap(find.byIcon(Icons.add_circle));
+    await tester.pump();
+
+    expect(find.byIcon(Icons.remove_circle), findsOneWidget);
+  });
+
+  testWidgets('Removes rows', (WidgetTester tester) async {
+    await tester.pumpWidget(RandomPickerApp());
+
+    expect(find.byElementType(TextFormField), findsNothing);
+
+    await tester.tap(find.byIcon(Icons.add_circle));
+    await tester.pump();
+
+    expect(find.byType(TextFormField), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.remove_circle));
+    await tester.pump();
+
+    expect(find.byElementType(TextFormField), findsNothing);
   });
 }
